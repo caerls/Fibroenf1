2fbd9692213ac57ab10d3379a3dfd89a328b858f
caerls <151209669+caerls@users.noreply.github.com>
2023-11-20 22:45:22 -0400

    Add files via upload

---
+++ js/masitas.js
@@ -0,0 +1,57 @@
+
+
+const macitas = [
+  {
+    id:1,
+    nombre: "Alfajores",
+    precio: 2.5,
+  },
+  {
+    id:2,
+    nombre: "Churros",
+    precio: 4,
+  },
+  {
+    id:3,
+    nombre: "Conos",
+    precio: 2,
+  },
+  {
+    id:4,
+    nombre: "Croasont",
+    precio: 4,
+  },
+  {
+    id:5,
+    nombre: "empanada",
+    precio: 5,
+  },
+  {
+    id:6,
+    nombre: "medialuna",
+    precio: 10,
+  },
+  {
+    id:7,
+    nombre:"donas",
+    precio:3,
+  },
+  {
+    id:8,
+    nombre:"donas rellenas",
+    precio:5,
+  },
+  {
+    id:9,
+    nombre:"Browni",
+    precio:6,
+  },
+
+  {id:10,
+    nombre:"cuñape",
+    precio:3,
+  }
+  
+
+
+]
